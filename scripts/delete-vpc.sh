e2cb0dfdaaa9b224a14af49dd4aad25535e0d020
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-11-19 16:33:28 +0100

    see last push

--- scripts/delete-vpc.sh
+++ scripts/delete-vpc.sh
@@ -5,7 +5,7 @@ export RESOURCE_GROUP=default
 export REGION="us-south"
 export VPC_NAME="my-example-vpc"
 export VPC_ID=""
-export FIRST_ZONE=""
+export SUBNET_NAME="my-example-subnet-1"
 export DEFAULT_NETWORK_ACL="my-default-example-acl"
 export DEFAULT_ROUTING_TABLE="my-default-example-routing-table"
 export DEFAULT_SECURITY_GROUP="my-default-example-security-group"
@@ -21,7 +21,7 @@ export TMP_VPC_CONFIG=tmp-vpc-configuration.json
 # **********************************************************************************
 
 function getVPCconfig() {
-
+    echo "Get the configuration for the Virtual Private Cloud $VPC_NAME"
     ibmcloud is vpc $VPC_NAME  --output JSON > $TMP_VPC_CONFIG
     VPC_ID=$(cat ./$TMP_VPC_CONFIG | jq '.id' | sed 's/"//g')
 
@@ -30,12 +30,21 @@ function getVPCconfig() {
     DEFAULT_SECURITY_GROUP_ID=$(cat ./$TMP_VPC_CONFIG | jq '.default_security_group.id' | sed 's/"//g')
     FIRST_ZONE=$(cat ./$TMP_VPC_CONFIG | jq '.cse_source_ips[0].zone.name' | sed 's/"//g')
  
-    # rm -f $TMP_VPC_CONFIG   
+    rm -f $TMP_VPC_CONFIG   
 }
 
 function deleteVPC() {
+    
+    echo "-> Delete Subnet: $SUBNET_NAME"
+    ibmcloud is subnet-delete $SUBNET_NAME --vpc $VPC_ID 
+    
+    echo "-> Delete Public Gateway : $PUBLIC_GATEWAY"
     ibmcloud is public-gateway-delete $PUBLIC_GATEWAY --vpc $VPC_ID  --force
+    
+    echo "-> Verify the Public Gateway $PUBLIC_GATEWAY doesn't exists anymore"
     ibmcloud is public-gateway $PUBLIC_GATEWAY --vpc $VPC_ID
+    
+    echo "-> Delete Virtual Private Cloud"
     ibmcloud is vpc-delete $VPC_ID --force
 }
 
